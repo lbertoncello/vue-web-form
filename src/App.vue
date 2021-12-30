65f30488338317e19df2f473a45dba7ad10ce2ce
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-30 18:41:44 -0300

    feat(App): addd SignupForm

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
+  <SignupForm />
 </template>
 
 <script>
+import SignupForm from './components/SignupForm.vue'
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    SignupForm
+  }
 }
 </script>
 
